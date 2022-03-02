33f34683efe82616e600c29dad3cd422c663e5fb
Joh E <johannes.ehala@ttu.ee>
2022-03-02 09:28:39 +0200

    Added links to documentation.

--- app_main.c
+++ app_main.c
@@ -4,6 +4,27 @@
  * @brief   Communicates with TTTW labkit accelerometer over I2C protocol. Writes 
  *          results to log output.
  *
+ * MMA8653FC datasheet
+ * https://www.nxp.com/docs/en/data-sheet/MMA8653FC.pdf
+ * 
+ * MMA8653FC application note
+ * https://www.nxp.com/docs/en/application-note/AN4083.pdf
+ * 
+ * EFR32 Application Note on I2C
+ * https://www.silabs.com/documents/public/application-notes/AN0011.pdf
+ *
+ * EFR32MG12 Wireless Gecko Reference Manual (I2C p501)
+ * https://www.silabs.com/documents/public/reference-manuals/efr32xg12-rm.pdf
+ * 
+ * EFR32MG12 Wireless Gecko datasheet
+ * https://www.silabs.com/documents/public/data-sheets/efr32mg12-datasheet.pdf
+ *
+ * GPIO API documentation 
+ * https://docs.silabs.com/mcu/latest/efr32mg12/group-GPIO
+ * 
+ * ARM RTOS API
+ * https://arm-software.github.io/CMSIS_5/RTOS2/html/group__CMSIS__RTOS.html
+ *
  * @author Johannes Ehala, ProLab.
  * @license MIT
  *
